b3459a9f7d93645c72c2d51b90e637545d04e2e9
diegoposba <diegoposba@gmail.com>
2025-05-19 09:00:03 -1000

    refactor: changement de nom de variable lidar maplibre-deckgl

--- src/components/lidar/lidar-map-gl.tsx
+++ src/components/lidar/lidar-map-gl.tsx
@@ -24,6 +24,7 @@ function DeckGLOverlay(props: DeckProps) {
 
 type DataProps = {
   position: [x: number, y: number, z: number];
+  normal?: [x: number, y: number, z: number];
   color: [r: number, g: number, b: number];
 };
 
