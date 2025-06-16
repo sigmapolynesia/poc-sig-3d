ca7d62da1b794e120ca7cf791d3da0d6fdb7e863
diegoposba <diegoposba@gmail.com>
2025-06-16 12:41:17 -1000

    add: dem de tahiti

--- src/components/config-ml.ts
+++ src/components/config-ml.ts
@@ -1,4 +1,4 @@
-export const DEM_URL = 'http://localhost:3000/assets/moorea/tiles.json';
+export const DEM_URL = 'http://localhost:3000/assets/tahiti/tiles.json';
 export const GEOJSON_URL = 'http://localhost:3000/assets/test.geojson';
 export const WMTS_URL = 'https://www.tefenua.gov.pf/api/wmts';
 export const MVT_URL = {
