79de2af4675157bb317038f694771072a886d7cc
diegoposba <diegoposba@gmail.com>
2025-06-16 10:14:45 -1000

    refactor: simplification du code initial de wmts pour le composant MultiLayer et ajout du dossier MNT Moorea dans le gitignore

--- src/components/MultiLayer.tsx
+++ src/components/MultiLayer.tsx
@@ -2,90 +2,62 @@ import React, { useEffect, useRef } from 'react';
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import MapContainer from './MapContainer';
-import WMTSLayerSelector from './wmts/wmts-layer-selector';
 import { useWMTS } from '../hooks/useWMTS';
+import { DEM_URL, GEOJSON_URL, WMTS_URL } from './config-ml';
 import { configureMapLibreWMTS, maplibreCenter } from '../utils/maplibre-utils';
 
-interface WMTSMapGLProps {
+interface LayerProps {
   center?: [number, number];
   zoom?: number;
   selectedLayer?: string;
 }
 
-const MultiLayer: React.FC<WMTSMapGLProps> = ({
+const MultiLayer: React.FC<LayerProps> = ({
   center = [-149.57, -17.67],
   zoom = 9.15,
   selectedLayer = 'TEFENUA:FOND'
 }) => {
   const mapContainer = useRef<HTMLDivElement>(null);
-  const map = useRef<maplibregl.Map | null>(null);
-  
-  const WMTS_URL = 'https://www.tefenua.gov.pf/api/wmts';
+  const mapRef = useRef<maplibregl.Map | null>(null);
   
   const { 
     layers, 
     currentLayer, 
-    setCurrentLayer,
-    isLoading 
   } = useWMTS(
     WMTS_URL, 
     selectedLayer,
-    (layers) => {
-      const filteredLayers = layers.filter(layer => layer.title === "Fond Léger" || layer.title === "FOND Tefenua" );
-
-      filteredLayers.forEach(layer => { layer.title = layer.title === "FOND Tefenua" ? "Fond Tefenua" : layer.title; });
-
-      return filteredLayers.sort((a) => {
-        if (a.title === "Fond Tefenua") return -1;
-        return 0;
-      });
-    }
   );
 
   // 1. Initialisation de la carte
   useEffect(() => {
-    if (!mapContainer.current || map.current) return;
+    if (!mapContainer.current || mapRef.current) return;
 
-    map.current = new maplibregl.Map({
+    const map = new maplibregl.Map({
       container: mapContainer.current,
       style: { version: 8, sources: {}, layers: [] },
       center,
       zoom
     });
 
-    return () => {
-      map.current?.remove();
-      map.current = null;
-    };
+    mapRef.current = map;
+    map.addControl(new maplibregl.NavigationControl());
   }, []);
 
   // 2. Chargement et mise à jour de la couche WMTS
   useEffect(() => {
-    if (!map.current || !map.current.loaded() || layers.length === 0) return;
+    if (!mapRef.current) return;
     
     const layer = layers.find(l => l.identifier === currentLayer);
     if (!layer) return;
     
-    configureMapLibreWMTS(map.current, layer, WMTS_URL);
+    configureMapLibreWMTS(mapRef.current, layer, WMTS_URL);
     
     // 3. Zoom sur la vue par défaut
-    maplibreCenter(map.current, zoom);
-  }, [currentLayer, layers, map.current?.loaded()]);
-
-  const reloadCurrentLayer = () => {
-    if (!map.current || !map.current.loaded() || layers.length === 0) return;
-    
-    const layer = layers.find(l => l.identifier === currentLayer);
-    if (!layer) return;
-    
-    configureMapLibreWMTS(map.current, layer, WMTS_URL);
-  };
+    maplibreCenter(mapRef.current, zoom);
+  }, [currentLayer, layers]);
 
   return (
     <div>
-      <div style={{ marginBottom: '10px' }}>
-        <WMTSLayerSelector layers={layers} currentLayer={currentLayer} onLayerChange={setCurrentLayer} onRefresh={reloadCurrentLayer} loading={isLoading} />
-      </div>
       <MapContainer ref={mapContainer} />
     </div>
   );
---
+++ src/components/config-ml.ts
@@ -0,0 +1,3 @@
+export const DEM_URL = 'http://localhost:3000/assets/moorea/tiles.json';
+export const GEOJSON_URL = 'http://localhost:3000/assets/test.geojson';
+export const WMTS_URL = 'https://www.tefenua.gov.pf/api/wmts';
